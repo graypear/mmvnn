27a92f419089c28bf648b4b9a1441d6fa475140c
bxqiujie <bxqiejie@163.com>
2017-12-21 16:50:04 +0800

    2017-12-21

---
+++ application/index/controller/Tests.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/21
+ * Time: 15:06
+ */
+
+namespace app\index\controller;
+use think\Controller;
+
+class Tests extends Controller
+{
+    public function index(){
+
+        return view();
+    }
+}
